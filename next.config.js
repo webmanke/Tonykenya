8303954ebab06c1508bd8de8af057cfcab6ca170
webmanke <166211809+webmanke@users.noreply.github.com>
2025-06-05 02:52:35 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  //experimental: {
-    //appDir: true,
-  },
+  swcMinify: true,
 };
+
+module.exports = nextConfig;
